71a69dd206f88a551047b1df3966219e6107595d
PauletteValle <143724538+PauletteValle@users.noreply.github.com>
2023-11-07 08:23:20 -0600

    Add files via upload

---
+++ U4ArreglosBidimensionales.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package u4arreglosbidimensionales;
+
+/**
+ *
+ * @author Usuario
+ */
+public class U4ArreglosBidimensionales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [][] numeros = new int [4][5];
+        int numero = 1;
+        
+        //Guardando informacion en arreglo unidimencional
+        for (int i = 0; i < 4; i++) { //7Control de filas
+            for (int j = 0; j < 5 ; j++) { //Contro de columnas
+                numeros[i][j] = numero;
+                numero++;
+                
+            }
+        }
+        //Leyendo informacion en arreglo unidimencional
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(numeros[i][j] + " ");
+                
+            }
+             System.out.println();
+        }
+    }
+    
+}
